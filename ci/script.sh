8108a697a10c161cef65b238a8a9fa391e414c1b
Michael Howell <michael@notriddle.com>
2017-10-11 18:23:11 -0700

    Enable building the binaries for Windows, Mac OS X, Linux, and FreeBSD
    Uses the `trust` pre-built configuration.

---
+++ ci/script.sh
@@ -0,0 +1,24 @@
+# This script takes care of testing your crate
+
+set -ex
+
+pushd server/
+
+main() {
+
+    cross build --target $TARGET
+
+    if [ ! -z $DISABLE_TESTS ]; then
+        return
+    fi
+
+    cross test --target $TARGET
+
+}
+
+# we don't run the "test phase" when doing deploys
+if [ -z $TRAVIS_TAG ]; then
+    main
+fi
+
+popd
